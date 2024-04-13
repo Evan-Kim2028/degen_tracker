39910eee842988c7df0cd50ac0fc837a63a09168
Evan K <ekcopersonal@gmail.com>
2024-04-13 13:45:05 -0400

    feat: add decoded logs output

--- src/degen_tracker/hypersync.py
+++ src/degen_tracker/hypersync.py
@@ -29,51 +29,108 @@ class Hypersync:
             # Convert hex string to float
             return float(int(hex, 16))
 
-    def sync_erc20s(self, block: int = 0) -> dict[str]:
-        return asyncio.run(self.fetch_erc20s())
+    def sync_erc20s(self, sync_all=False) -> dict[str]:
+        """
+        sync_erc20s() is a synchronous wrapper function around the asynchronous fetch_erc20s() function.
 
-    async def fetch_erc20s(self) -> dict[str]:
+        sync is a boolean value that determines whether to sync all erc20 transfers from block 0 or from the latest block. It is False by default. 
+        If false, it will simply sync to the block head.
+
+        Returns:
+            dict: A dictionary containing the following keys:
+                {
+                    "tx_data": tx_data,
+                    "decoded_log_data": decoded_log_data,
+                    "log_data": log_data,
+                    "block_data": block_data
+                }
+        """
+        match sync_all:
+            case True:
+                return asyncio.run(self.fetch_erc20s(sync_all=True))
+            case False:
+                return asyncio.run(self.fetch_erc20s(sync_all=False))
+
+    async def fetch_erc20s(self, sync_all=False) -> dict[str]:
+        """
+        - TODO get latest block header from lance database and update
+
+        Returns:
+            dict: A dictionary containing the following keys:
+                {
+                    "tx_data": tx_data,
+                    "decoded_log_data": decoded_log_data,
+                    "log_data": log_data,
+                    "block_data": block_data
+                }
+        """
         # Get the current block height from the blockchain.
         height = await self.client.get_height()
 
-        # The query to run
-        query = hypersync.Query(
-            # Full sync
-            # from_block=0,
-            # to_block=height,
-            from_block=height - 1000,
-            logs=[hypersync.LogSelection(
-                # We want All ERC20 transfers so no address filter and only a filter for the first topic
-                topics=[
-                    ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]]
-            )],
-            # Select the fields we are interested in, notice topics are selected as topic0,1,2,3
-            field_selection=hypersync.FieldSelection(
-                log=[el.value for el in LogField],
-                transaction=[el.value for el in TransactionField],
-                block=[el.value for el in hypersync.BlockField]
-            )
-        )
+        match sync_all:
+            case True:
+                query = hypersync.Query(
+                    # Full sync
+                    from_block=0,
+                    to_block=height,
+                    logs=[hypersync.LogSelection(
+                        # We want All ERC20 transfers so no address filter and only a filter for the first topic
+                        topics=[
+                            ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]]
+                    )],
+                    field_selection=hypersync.FieldSelection(
+                        log=[el.value for el in LogField],
+                        transaction=[TransactionField.BLOCK_NUMBER,
+                                     TransactionField.TRANSACTION_INDEX,
+                                     TransactionField.HASH,
+                                     TransactionField.FROM,
+                                     TransactionField.TO
+                                     ],
+                        block=[el.value for el in hypersync.BlockField]
+                    )
+                )
+            case False:
+                query = hypersync.Query(
+                    # Full sync
+                    # from_block=0,
+                    # to_block=height,
+                    from_block=height - 3000,
+                    logs=[hypersync.LogSelection(
+                        # We want All ERC20 transfers so no address filter and only a filter for the first topic
+                        topics=[
+                            ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"]]
+                    )],
+                    field_selection=hypersync.FieldSelection(
+                        log=[el.value for el in LogField],
+                        transaction=[TransactionField.BLOCK_NUMBER,
+                                     TransactionField.TRANSACTION_INDEX,
+                                     TransactionField.HASH,
+                                     TransactionField.FROM,
+                                     TransactionField.TO
+                                     ],
+                        block=[el.value for el in hypersync.BlockField]
+                    )
+                )
 
         print("Running the query...")
 
+        # DATA ORGANIZTION
         tx_data = []
+        decoded_log_data = []
         log_data = []
         block_data = []
-        # Continuously fetch data until the end of the specified period is reached.
+
+        # While loop for pagination
         while True:
-            # Send the query to the blockchain client.
             res = await self.client.send_req(query)
 
-            # read json abi file for erc20
+            # ABI is required to decode logs
             with open('./abis/erc20.json', 'r') as json_file:
                 abi = json_file.read()
 
             # Map of contract_address -> abi
             abis = {}
 
-            # every log we get should be decodable by this abi but we don't know
-            # the specific contract addresses since we are indexing all erc20 transfers.
             for log in res.data.logs:
                 abis[log.address] = abi
 
@@ -84,32 +141,26 @@ class Hypersync:
             # Can also use decoder.decode_logs_sync if it is more convenient.
             decoded_logs = await decoder.decode_logs(res.data.logs)
 
-            # Append the fetched transactions and log to their respective lists.
             tx_data += res.data.transactions
-            log_data += decoded_logs
+            decoded_log_data += decoded_logs
+            log_data = res.data.logs
             block_data += res.data.blocks
 
+            print('out of for loop?')
             # Check if the fetched data has reached the current archive height or next block.
             if res.archive_height < res.next_block:
-                # Exit the loop if the end of the period (or the blockchain's current height) is reached.
                 break
 
             # Update the query to fetch the next set of data starting from the next block.
             query.from_block = res.next_block
             print(f"Scanned up to block {query.from_block}")  # Log progress.
 
-            # ! log data structure1
-            # print(log_data[0])
-            # print(log_data[0].body)
-            # print(log_data[0].indexed)
-
-            # print(res.data.transactions[0])
-            # print(res.data.blocks[0])
             print("# of logs", len(log_data))
             print('# of blocks', len(block_data))
 
             return {
                 "tx_data": tx_data,
+                "decoded_log_data": decoded_log_data,
                 "log_data": log_data,
                 "block_data": block_data
             }
