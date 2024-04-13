b5b9894fb2912f387d31f379cbcf733b320e7281
Evan K <ekcopersonal@gmail.com>
2024-04-13 14:56:11 -0400

    feat: add lancedb log table syncing

--- src/degen_tracker/lance.py
+++ src/degen_tracker/lance.py
@@ -1,3 +1,85 @@
+import datetime
+import lancedb
+import polars as pl
+import time
+
+from dataclasses import dataclass
+from degen_tracker.hypersync import Hypersync
 
 
-# TODO lance database management here
+@dataclass
+class LanceDBLogs:
+    # 1. connect to lancedb and initialize a lance table an initial query
+    uri: str = "logs"
+    db: lancedb.DBConnection = lancedb.connect(uri)
+    # error when trying to create table, it already exists.
+    logs_tbl = None
+
+    def __post_init__(self):
+        # Check if database exists. If it does, then update logs_tbl to point to the existing database
+        if self.db is None:
+            self.logs_tbl = None
+        else:
+            self.logs_tbl = self.db.open_table(self.uri)
+
+    def initial_db_sync(self, full_sync=False):
+        """
+        Call this function first to create the database and sync the logs table. 
+
+        full_sync=False defaults to a 10000 block range sync. Use this to get started.
+        full_sync=True if you want to sync the entire history.
+
+        Use full_sync=False
+        """
+        client = Hypersync()
+
+        match full_sync:
+            case True:
+                erc20_logs_df: pl.DataFrame = client.get_erc20_df(
+                    sync_all=full_sync)
+            case False:
+                # sync to head with a 1000 range
+                erc20_logs_df: pl.DataFrame = client.get_erc20_df(
+                    sync_all=full_sync, block_num_range=10000)
+
+        try:
+            # Attempt to create the table if it doesn't exist
+            self.logs_tbl = self.db.create_table(
+                self.uri, data=erc20_logs_df)
+        except OSError as e:
+            # Check if the error message is about the dataset already existing
+            if "Dataset already exists" in str(e):
+                # Skip table creation because it already exists
+                print("Table already exists, skipping creation.")
+            else:
+                # If the error is due to another reason, re-raise the exception
+                raise
+
+    def update_db(self, refresh_rate: int = 5):
+        """
+        Constantly streams new data to update the logs database.
+        `refresh_rate` is the number of seconds to wait before the next call.
+
+        This function assumes that the initial_db_sync has already been called.
+        """
+        client = Hypersync()
+
+        while True:
+            erc20_logs_df: pl.DataFrame = client.get_erc20_df()
+
+            # Perform a "upsert" operation
+            self.logs_tbl.merge_insert("block_number")   \
+                .when_not_matched_insert_all() \
+                .execute(erc20_logs_df)
+
+            # lance db cleanup
+            # make table fragments compact
+            # 1m target rows per file
+            self.logs_tbl.compact_files(target_rows_per_fragment=1000000)
+
+            self.logs_tbl.cleanup_old_versions(
+                older_than=datetime.timedelta(seconds=30), delete_unverified=True
+            )
+
+            print(f'sleeping for {refresh_rate} seconds')
+            time.sleep(refresh_rate)
---
+++ test/start_lance.py
@@ -0,0 +1,13 @@
+from degen_tracker.lance import LanceDBLogs
+
+
+# initialize this dataclass, which will be used to build the logs database
+lance_logs = LanceDBLogs()
+
+
+# initial db sync. If the DB already exists, then this will be skipped.
+lance_logs.initial_db_sync(full_sync=False)
+
+print('STARTING TO UPDATE DB CONTINUOUSLY')
+# update the logs database every 5 seconds.
+lance_logs.update_db(refresh_rate=5)
